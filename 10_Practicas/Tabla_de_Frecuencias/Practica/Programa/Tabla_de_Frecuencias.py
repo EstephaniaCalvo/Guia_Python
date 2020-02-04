22168fa8486b1f4cee4200be6b6780fcce3563b2
EstephaniaCalvo <57077216+EstephaniaCalvo@users.noreply.github.com>
2020-02-03 20:07:12 -0500

    Correciones varias
    Se realizan correcciones de redacción en la práctica de tablas de frecuencias, Home y Estructura de datos

--- 10_Practicas/Tabla_de_Frecuencias/Practica/Programa/Tabla_de_Frecuencias.py
+++ 10_Practicas/Tabla_de_Frecuencias/Practica/Programa/Tabla_de_Frecuencias.py
@@ -1,20 +1,16 @@
 # Título: Tabla de Frecuencias
-# Autor: Estephania Calvo Carvajal
+# Autor: 
 
 # -2. Importar módulos
 # Pase aquí los módulos utilizados.
 
 # -1. Funciones auxiliares 
-
-## Pase aquí las funciones auxiliares
-
-## a. Leer y almacenar datos de entrada.
-
+# Pase aquí las funciones auxiliares.
 
 # 0. Programa principal
 def main():
-    
-	# Pase aquí el programa principal
+
+    # Pase aquí el programa principal
         
 if __name__=='__main__':
     main()
